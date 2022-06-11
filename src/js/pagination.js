0ca644ff2f103b21bf345e7752362c917f0818db
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-06-11 13:38:10 -0500

    add_jsdoc

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,7 +4,7 @@ let currentPage = 0;
 function pagination(id) {
   if (id === 'next') currentPage++;
   if (id === 'prev') currentPage--;
-  /* al presionar next se asigna como currenpage y no deberia */
+
   if (id !== 'next' && id !== 'prev') currentPage = id;
   getProductsList(currentPage);
 }
